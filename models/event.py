d90b2bf7df3027024ff1d5d9237fcac241a42db1
Danny <danny.belmonte4@gmail.com>
2025-08-04 17:44:27 -0500

    updated event model

--- models/event.py
+++ models/event.py
@@ -19,14 +19,13 @@ class Event(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     event_number = Column(Integer, unique=True, autoincrement=True)
-    event_name = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     description = Column(String, nullable=True)
     location = Column(String, nullable=False)
-    event_datetime = Column(DateTime, nullable=False)
-    event_date = Column(Date, Computed("event_datetime::date", persisted=True))
+    date_time = Column(DateTime, nullable=False)
     host_id = Column(UUID(as_uuid=True), ForeignKey("hosts.id", ondelete="CASCADE"), nullable=False)
     created_at = Column(DateTime, server_default=func.now())
 
     __table_args__ = (
-        UniqueConstraint("host_id", "event_date", name="unique_host_per_day"),
+        UniqueConstraint("host_id", "date_time", name="unique_host_per_day"),
     )
