4e17ae872e0847c7689c052bf0c1abb8fb2ec533
Danny <danny.belmonte4@gmail.com>
2025-08-07 18:43:16 -0500

    updated references to password_hash

--- schema/host_schemas.py
+++ schema/host_schemas.py
@@ -7,15 +7,15 @@ class HostCreateSchema(BaseModel):
     company_name: str
     email: EmailStr # ISO format date string
     password: str 
-    created_at: datetime  # ISO format date string
+    created_at: datetime  # ISO format date string // Do we need created_at fields here and below?
 
 class HostUpdateSchema(BaseModel):
     email: Optional[EmailStr] = None
     company_name: Optional[str] = None
-    password_hash: Optional[str] = None
+    password: Optional[str] = None
     created_at: Optional[datetime] = None
 
-# Schema used for reading/returning a host (e.g., in responses)
+# Schema used for reading/returning a host (e.g., in responses) // ??
 class HostReadSchema(BaseModel):
     id: UUID
     host_number: int
