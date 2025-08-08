8121e2997dc3fdad2d1bd61e245fbd4763c9062d
Danny <danny.belmonte4@gmail.com>
2025-08-08 13:32:11 -0500

    removed created_at fields (create and update) and password_hash from response

--- schema/host_schemas.py
+++ schema/host_schemas.py
@@ -7,21 +7,20 @@ class HostCreateSchema(BaseModel):
     company_name: str
     email: EmailStr # ISO format date string
     password: str 
-    created_at: datetime  # ISO format date string // Do we need created_at fields here and below?
 
 class HostUpdateSchema(BaseModel):
     email: Optional[EmailStr] = None
     company_name: Optional[str] = None
     password: Optional[str] = None
-    created_at: Optional[datetime] = None
 
-# Schema used for reading/returning a host (e.g., in responses) // ??
+# Schema used for reading/returning a host 
 class HostReadSchema(BaseModel):
     id: UUID
     host_number: int
-    password_hash: str
     created_at: datetime 
     company_name: str
     email: EmailStr
     class Config:
         from_attributes = True 
+
+    
