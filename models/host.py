2ef25b0f8928daeda6e81fef675ad2d1aa7ec228
Danny <danny.belmonte4@gmail.com>
2025-08-07 19:22:51 -0500

    replaced manual seq in host model width Identity

--- models/host.py
+++ models/host.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, Integer, UniqueConstraint
+from sqlalchemy import Column, String, DateTime, Integer, UniqueConstraint, Identity
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 from database.session import Base
@@ -8,8 +8,8 @@ class Host(Base):
     __tablename__ = "hosts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    host_number = Column(Integer, unique=True, autoincrement=True, server_default=func.nextval('host_number_seq'))
+    host_number = Column(Integer, Identity(start=1, increment=1), unique=True, nullable=False)
     company_name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
