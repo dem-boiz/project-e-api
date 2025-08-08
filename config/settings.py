753ed1e10050a14dd95e98e8a50e172a6c2d68fe
Danny <danny.belmonte4@gmail.com>
2025-08-08 13:57:13 -0500

    minor changes

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ALGORITHM = os.getenv("ALGORITHM", "HS256") # TODO Change to RS256 in future for better security?
 OTP_EXPIRY_SECONDS = int(os.getenv("OTP_EXPIRY_SECONDS", 300))
 DATABASE_URL = os.getenv("DATABASE_URL")
 
--- services/__init__.py
+++ services/__init__.py
@@ -3,3 +3,4 @@ from .otp_service import OTPService
 from .event_service import EventService
 from .host_service import HostService   
 from .user_event_access_service import UserEventAccessService
+from .auth_service import AuthService
