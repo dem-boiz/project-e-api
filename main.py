e75210c33ecf898b6d83dc695ab45d4e0eadba4a
Danny <danny.belmonte4@gmail.com>
2025-08-11 11:10:54 -0500

    set allow credentials to true for cors, needed for auth

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins for development
     allow_origin_regex="http://localhost.*",
-    allow_credentials=False, # Will need to set to true when we implement authentication
+    allow_credentials=True, # Will need to set to true when we implement authentication
     allow_methods=["*"],
     allow_headers=["*"],
 )
