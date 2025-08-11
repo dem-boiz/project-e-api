cd98273aaa2a36239ea2ae2d780cf31cafb717f6
Danny <danny.belmonte4@gmail.com>
2025-08-11 15:48:55 -0500

    RefreshResponse body fix

--- schema/auth_schemas.py
+++ schema/auth_schemas.py
@@ -20,9 +20,7 @@ class LoginResponse(BaseModel):
     refresh_token: str
 class RefreshResponse(BaseModel):
     access_token: str
-    refresh_token: str
     token_type: str = "bearer"
-
     class Config:
         from_attributes = True
 
