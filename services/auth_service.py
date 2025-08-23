efdc2ce9cd0a7726f613acd9d5811c7e92144602
Danny <danny.belmonte4@gmail.com>
2025-08-23 09:21:11 -0500

    removed refresh_token from response body, (security vulnerability)

--- services/auth_service.py
+++ services/auth_service.py
@@ -353,7 +353,6 @@ class AuthService:
             "access_token": access_token,
             "token_type": "bearer",
             "csrf_token": new_csrf_token,
-            "refresh_token": refresh_token
         }
         
     async def kill_session_service(self, 
