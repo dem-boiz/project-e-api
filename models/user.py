593809c6882ee11935b0a68c464e72961380186d
Simon <sacost6@uic.edu>
2025-08-29 17:01:34 -0500

    Updated added

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, Boolean, text
+from sqlalchemy import Column, Identity, Integer, String, DateTime, Boolean, text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 import uuid
@@ -12,9 +12,13 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_number: Mapped[int] = mapped_column(Integer, Identity(start=1, increment=1), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    password_hash: Mapped[str] = mapped_column(String, nullable=False) 
     is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text('false'))
-
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text('true')) 
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    update_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    
     # Use string reference instead of class reference
     #sessions = relationship("Session", back_populates="users", cascade="all, delete-orphan")
