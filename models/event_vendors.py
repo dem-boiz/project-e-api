9e08d4ee0add622318997d00b45cb7f6c289763d
Simon <sacost6@uic.edu>
2025-08-29 23:53:29 -0500

    Added images and descriptions to individual event_vendor records

--- models/event_vendors.py
+++ models/event_vendors.py
@@ -1,6 +1,10 @@
+from typing import List, Optional
 from sqlalchemy import (
+    ARRAY,
     DateTime,
     ForeignKey,
+    LargeBinary,
+    Text,
     UniqueConstraint,
 )
 from sqlalchemy.dialects.postgresql import UUID
@@ -18,5 +22,7 @@ class EventVendor(Base):
     event_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey(Event.__table__.c.id), primary_key=True)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey(User.__table__.c.id), primary_key=True)
     event_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    vendor_description: Mapped[Optional[str]] = mapped_column(Text, nullable=True, server_default=None)
+    vendor_images: Mapped[Optional[List[bytes]]] = mapped_column(ARRAY(LargeBinary), nullable=True, server_default=None)
     added_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
  
