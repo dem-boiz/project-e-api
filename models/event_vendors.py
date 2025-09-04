201fe62a8a69813b378486990b4cc57fb6b921cc
dbelmo2 <49499409+dbelmo2@users.noreply.github.com>
2025-09-04 12:05:20 -0500

    Remove event_date column from event_vendors table

--- models/event_vendors.py
+++ models/event_vendors.py
@@ -21,7 +21,6 @@ class EventVendor(Base):
 
     event_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey(Event.__table__.c.id), primary_key=True)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey(User.__table__.c.id), primary_key=True)
-    event_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     vendor_description: Mapped[Optional[str]] = mapped_column(Text, nullable=True, server_default=None)
     vendor_images: Mapped[Optional[List[bytes]]] = mapped_column(ARRAY(LargeBinary), nullable=True, server_default=None)
     added_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
