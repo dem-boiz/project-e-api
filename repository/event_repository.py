0f1aac924cacc42f7a60af20632a9b911165d0ed
Danny <danny.belmonte4@gmail.com>
2025-09-10 14:01:28 -0500

    updated to return read schema instead of db object

--- repository/event_repository.py
+++ repository/event_repository.py
@@ -6,11 +6,13 @@ import uuid
 from models import Event
 from schema import EventCreateSchema, EventUpdateSchema
 from datetime import datetime
+
+from schema.event_schemas import EventReadSchema
 class EventRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create_event(self, event: EventCreateSchema, host_id: uuid.UUID) -> Event:
+    async def create_event(self, event: EventCreateSchema, host_id: uuid.UUID) -> EventReadSchema:
         # Clean the datetime string and parse it
         #parsed_datetime = datetime.fromisoformat(event.datetime.replace('Z', '+00:00'))
         
@@ -27,33 +29,37 @@ class EventRepository:
         self.session.add(new_event)
         await self.session.commit()
         await self.session.refresh(new_event)
-        return new_event
+        return EventReadSchema.model_validate(new_event)
 
-    async def get_all_events(self) -> Sequence[Event]:
+    async def get_all_events(self) -> Sequence[EventReadSchema]:
         result = await self.session.execute(select(Event))
         events = result.scalars().all()
-        return events
-    
-    async def get_event_by_name(self, event_name: str):
+        return [EventReadSchema.model_validate(event) for event in events]
+
+    async def get_event_by_name(self, event_name: str) -> EventReadSchema | None:
         result = await self.session.execute(
             select(Event).where(Event.name == event_name)
         )
         event = result.scalar_one_or_none()
 
-        return event
-    async def get_event_by_id(self, event_id: uuid.UUID):
+        return EventReadSchema.model_validate(event) if event else None
+
+    async def get_event_by_id(self, event_id: uuid.UUID) -> EventReadSchema | None:
         result = await self.session.execute(
             select(Event).where(Event.id == event_id)
         )
         event = result.scalar_one_or_none()
+        if event:
+            return EventReadSchema.model_validate(event)
+        return None
 
-        return event
-    async def get_events_by_ids(self, event_ids: list[uuid.UUID]) -> Sequence[Event]:
+    async def get_events_by_ids(self, event_ids: list[uuid.UUID]) -> Sequence[EventReadSchema]:
         result = await self.session.execute(
             select(Event).where(Event.id.in_(event_ids))
         )
         events = result.scalars().all()
-        return events
+        event_schemas = [EventReadSchema.model_validate(event) for event in events]
+        return event_schemas
 
     async def delete_event(self, event_id: uuid.UUID) -> bool:
         try:
@@ -65,7 +71,7 @@ class EventRepository:
         except NoResultFound:
             return False
         
-    async def update_event(self, event_id: uuid.UUID, data: EventUpdateSchema) -> Event | None:
+    async def update_event(self, event_id: uuid.UUID, data: EventUpdateSchema) -> EventReadSchema | None:
         try:
             result = await self.session.execute(select(Event).where(Event.id == event_id))
             event = result.scalar_one()
@@ -82,6 +88,6 @@ class EventRepository:
 
             await self.session.commit()
             await self.session.refresh(event)
-            return event
+            return EventReadSchema.model_validate(event)
         except NoResultFound:
             return None
